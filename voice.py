5768a18b385b2a1e2d06f263097e8deb386ed9ac
Dmitriy <59870516+dmtep@users.noreply.github.com>
2021-11-16 22:49:56 +0200

    Пояснения

--- voice.py
+++ voice.py
@@ -3,7 +3,7 @@ import subprocess
 
 engine = pyttsx3.init()
 engine.setProperty("voice", "com.apple.speech.synthesis.voice.milena")
-
+# Заранее устанавливаем библиотеку ffmpeg-python в операционную систему.
 # ffmpeg -i test.mp3 -c:a libopus test_out.ogg
 # ["ffmpeg", '-i', audio_path_wav, '-acodec", 'libopus', audio_path_wav, '-y']
 # При входящем  текстовом сообщении, модуль subprocess.run позволяет создавать новые процессы,
